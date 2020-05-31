97f4d6262a8abe17737fcc11d96f990a10d28010
akhettar <ayache@openconsult.digital>
2020-05-31 15:42:46 +0300

    testing

--- action.go
+++ action.go
@@ -35,7 +35,7 @@ func NewReadinessCheck() *ReadinessCheck {
 	}
 
 	// url, ok := os.LookupEnv(InputReadinessEndpiont)
-	url, ok := os.LookupEnv("ACTIONS_RUNTIME_URL")
+	url, ok := os.LookupEnv("GITHUB_SERVER_URL")
 	if !ok {
 		log.Fatal("the readiness endpoint must be provided")
 	}
